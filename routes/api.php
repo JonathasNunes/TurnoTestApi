2ce8fda8828d85f1b513dfa5c554200751178809
Jonathas Nunes <jonathas.nunes@me.com>
2023-08-22 13:24:52 -0300

    Update - middleware para rota

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('auth/login', 'App\Http\Controllers\AuthController@login');
 Route::post('user', 'App\Http\Controllers\UserController@store');
-Route::get('users', 'App\Http\Controllers\UserController@index');
 
 Route::group(['middleware' => ['apiJwt']], function() {
 
@@ -23,7 +22,7 @@ Route::group(['middleware' => ['apiJwt']], function() {
     Route::post('auth/refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('auth/me', 'App\Http\Controllers\AuthController@me');
 
-    //Route::get('users', 'App\Http\Controllers\UserController@index');
+    Route::get('users', 'App\Http\Controllers\UserController@index');
 
     Route::post('transaction', 'App\Http\Controllers\TransactionController@store');
     Route::put('transaction', 'App\Http\Controllers\TransactionController@update');
